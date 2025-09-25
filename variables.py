111069f90471d512377daa65137eca8c666d4b1f
hannahharshitha2405 <hannahharshitha2405@gmail.com>
2025-09-25 14:57:30 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,39 @@
+#declearing  the variables 
+x = 7
+#var do not need any particular type and can be changed
+x = "qury"
+#casting
+y=float(6)
+z= "blesso"
+print(x)
+print(y)
+print(type(z))#variable with type function
+
+
+#here we go with the assign multile values
+x,y,z = "harshi","pranthi","dharmitha"
+print(x)
+print(y)
+print(z)
+# one value to multiple variable 
+u = y = f= "blesso"
+print(u)
+print(y)
+print(f)
+
+#unpack a collection
+friends = ["ram","hari","rajesh"]
+a,b,c = friends
+print(a)
+print(b)
+print(c)
+
+
+#output variables
+x="sathyabama in chennai"
+y="nellore"
+z="is"
+h="nice"
+print(x)
+print(y,z,h)
+print(y + z + h)
